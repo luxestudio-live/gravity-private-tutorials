b730d2efe1b273fff7da211ab943727759c00e0c
Rahul Kushwaha <rk.rahulkushwaha22@gmail.com>
2026-01-18 14:33:19 +0530

    fix: remove leading slashes from image paths for faculty members

--- app/faculty/page.tsx
+++ app/faculty/page.tsx
@@ -24,7 +24,7 @@ const facultyList: FacultyMember[] = [
 		experience: "",
 		specialization: "",
 		achievements: [],
-		image: "/ramkrishna-badgujar.jpeg",
+		image: "ramkrishna-badgujar.jpeg",
 		color: "from-secondary to-primary",
 	},
 	{
@@ -34,7 +34,7 @@ const facultyList: FacultyMember[] = [
 		experience: "",
 		specialization: "",
 		achievements: [],
-		image: "/sujeet-patil.png",
+		image: "sujeet-patil.png",
 		color: "from-accent to-secondary",
 	},
 	{
@@ -44,7 +44,7 @@ const facultyList: FacultyMember[] = [
 		experience: "",
 		specialization: "",
 		achievements: [],
-		image: "/pratik-sawant.jpeg",
+		image: "pratik-sawant.jpeg",
 		color: "from-accent to-secondary",
 	},
 	{
@@ -54,7 +54,7 @@ const facultyList: FacultyMember[] = [
 		experience: "16 Years",
 		specialization: "O.CM. / S.P",
 		achievements: [],
-		image: "/pravin-devidas-thorat.jpeg",
+		image: "pravin-devidas-thorat.jpeg",
 		color: "from-primary to-accent",
 	},
 	{
@@ -64,7 +64,7 @@ const facultyList: FacultyMember[] = [
 		experience: "",
 		specialization: "",
 		achievements: [],
-		image: "/santosh-gopal-sawant.jpeg",
+		image: "santosh-gopal-sawant.jpeg",
 		color: "from-secondary to-primary",
 	},
 	{
@@ -74,7 +74,7 @@ const facultyList: FacultyMember[] = [
 		experience: "11 Years",
 		specialization: "",
 		achievements: [],
-		image: "/arun-saheb-gauda.jpg",
+		image: "arun-saheb-gauda.jpg",
 		color: "from-primary to-accent",
 	},
 ]
