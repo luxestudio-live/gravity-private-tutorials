bbaa611abdb72309fbbb3e43830e8e9fe31b7ec2
Sonali Gore <sonaligore0906@gmail.com>
2026-02-17 22:55:51 +0530

    fix: update base path logic to account for custom domain in GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+const hasCustomDomain = process.env.CUSTOM_DOMAIN === 'true'
 const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'gravity-private-tutorials'
-const useRepoBasePath = isGithubActions
+const useRepoBasePath = isGithubActions && !hasCustomDomain
 
 const nextConfig = {
   typescript: {
