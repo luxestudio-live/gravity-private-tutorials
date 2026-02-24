30bbfa76508b4aed41d092b329e6cc0d4b0327f5
Sonali Gore <sonaligore0906@gmail.com>
2026-02-24 23:34:47 +0530

    Simplify next.config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
-const hasCustomDomain = process.env.CUSTOM_DOMAIN === 'true'
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'gravity-private-tutorials'
-const useRepoBasePath = isGithubActions && !hasCustomDomain
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -12,13 +7,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: useRepoBasePath ? `/${repositoryName}` : '',
-  },
-  basePath: useRepoBasePath ? `/${repositoryName}` : '',
-  assetPrefix: useRepoBasePath ? `/${repositoryName}/` : undefined,
-  trailingSlash: true,
-  output: 'export',
 }
 
 export default nextConfig
